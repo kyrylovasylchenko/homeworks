455b27005df4f5ac5baf949e7b7a5f0419c852f2
kyva22 <121287345+kyva22@users.noreply.github.com>
2023-03-19 23:57:14 +0200

    Delete SqlQuestionNotFoundException.java

--- src/main/java/VKS_12/exception/SqlQuestionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package VKS_12.exception;
-
-public class SqlQuestionNotFoundException extends RuntimeException{
-    public SqlQuestionNotFoundException(String message) {
-        super(message);
-    }
-}
