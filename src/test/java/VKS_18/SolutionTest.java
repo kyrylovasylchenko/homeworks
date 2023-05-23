1c496139f5d5e84fc146274f1c925d4d8be0f06e
kyva22 <tyrtyrforit@gmail.com>
2023-05-23 13:41:16 +0300

    VKS_18

---
+++ src/test/java/VKS_18/SolutionTest.java
@@ -0,0 +1,16 @@
+package VKS_18;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+
+    Solution solution = new Solution();
+    @Test
+    public void romanToIntTest(){
+
+        Assert.assertEquals(3, solution.romanToInt("III"));
+        Assert.assertEquals(58, solution.romanToInt("LVIII"));
+        Assert.assertEquals(1994, solution.romanToInt("MCMXCIV"));
+    }
+}
