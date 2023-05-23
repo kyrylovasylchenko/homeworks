cc028182c908894fd54e55c4183ed28246e83554
kyva22 <tyrtyrforit@gmail.com>
2023-05-23 14:06:37 +0300

    VKS_18

---
+++ src/main/java/VKS_20/Solution.java
@@ -0,0 +1,26 @@
+package VKS_20;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int romanToInt(String str) {
+        HashMap<Character,Integer> roman = new HashMap<>();
+        roman.put('I',1);
+        roman.put('V',5);
+        roman.put('X',10);
+        roman.put('L',50);
+        roman.put('C',100);
+        roman.put('D',500);
+        roman.put('M',1000);
+        int res = roman.get(str.charAt(str.length()-1));
+        for(int i=str.length()-2;i>=0;i--){
+            if(roman.get(str.charAt(i)) < roman.get(str.charAt(i+1))){
+                res -= roman.get(str.charAt(i));
+            }else{
+                res += roman.get(str.charAt(i));
+            }
+        }
+        return res;
+    }
+
+}
